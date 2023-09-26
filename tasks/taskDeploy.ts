8f723da8d6e783e1aafda5de4fcbb3d2e7b96f1f
Joe Henke <joe@foundation.app>
2023-09-25 21:29:56 -0700

    feat: add task:deployTicTacToe

--- tasks/taskDeploy.ts
+++ tasks/taskDeploy.ts
@@ -10,3 +10,11 @@ task("task:deployGreeter")
     await greeter.waitForDeployment();
     console.log("Greeter deployed to: ", await greeter.getAddress());
   });
+
+task("task:deployTicTacToe").setAction(async function (taskArguments: TaskArguments, { ethers }) {
+  const signers = await ethers.getSigners();
+  const tictactoeFactory = await ethers.getContractFactory("TicTacToe");
+  const tictactoe = await tictactoeFactory.connect(signers[0]).deploy();
+  await tictactoe.waitForDeployment();
+  console.log("Tic Tac Toe deployed to: ", await tictactoe.getAddress());
+});
