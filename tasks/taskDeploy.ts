30f7a8aefffc295b01d30f3cb6036853cff68514
Joe Henke <joe@foundation.app>
2023-09-28 18:14:13 -0700

    chore: clean up Greeter

--- tasks/taskDeploy.ts
+++ tasks/taskDeploy.ts
@@ -1,20 +1,12 @@
 import { task } from "hardhat/config";
 import type { TaskArguments } from "hardhat/types";
 
-task("task:deployGreeter")
-  .addParam("greeting", "Say hello, be nice")
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const signers = await ethers.getSigners();
-    const greeterFactory = await ethers.getContractFactory("Greeter");
-    const greeter = await greeterFactory.connect(signers[0]).deploy(taskArguments.greeting);
-    await greeter.waitForDeployment();
-    console.log("Greeter deployed to: ", await greeter.getAddress());
-  });
-
 task("task:deployTicTacToe").setAction(async function (taskArguments: TaskArguments, { ethers }) {
+  console.log("beginning deploy");
   const signers = await ethers.getSigners();
   const tictactoeFactory = await ethers.getContractFactory("TicTacToe");
   const tictactoe = await tictactoeFactory.connect(signers[0]).deploy();
   await tictactoe.waitForDeployment();
+  console.log("beginning deploy");
   console.log("Tic Tac Toe deployed to: ", await tictactoe.getAddress());
 });
